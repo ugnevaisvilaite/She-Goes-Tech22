a65b9d0681561ced621a1838f9e3d2fb68c55735
Ugne Vais <ugnevais88@inbox.lt>
2022-09-08 20:38:12 +0300

    class work day2

--- class_work_day2.py
+++ class_work_day2.py
@@ -8,3 +8,14 @@
 # currentYear = datetime.datetime.now (). yearLet the program also show the year when the user will be 100 years old.
 # It could use a variable with the current year.
 # It would be even better to get the current year automatically
+import datetime
+
+username = input ("What is your username? ")
+age = int(input (f"{username}, how old are you? "))
+mark_age = 100
+age_difference = mark_age - age
+print(f" Well today you are {age} years old. You will reach {mark_age} years old in {age_difference} years")
+
+currentYear = datetime.datetime.now().year # why "datetime" is twice?
+year = currentYear + age_difference
+print(f"You will be {mark_age} in year: {year}")
