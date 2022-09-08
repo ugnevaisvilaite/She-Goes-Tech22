8396ef435810c1f78a4380607e180fc78d6026c1
Ugne Vais <ugnevais88@inbox.lt>
2022-09-08 20:28:10 +0300

    ..

---
+++ class_work_day2.py
@@ -0,0 +1,10 @@
+# So today's exercise: #
+# Write a program that asks for and saves a username
+# Ask a question about the user's age, using the username in the question.
+# Shows in how many years the user will be 100 years old smile
+# BONUS: 
+# then you will need two additional lines:
+# import datetime # let's talk about imports separately
+# currentYear = datetime.datetime.now (). yearLet the program also show the year when the user will be 100 years old.
+# It could use a variable with the current year.
+# It would be even better to get the current year automatically
