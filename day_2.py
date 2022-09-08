cd6455ca2f0c5b0f6cd2432dfd71edc2c94fcade
Ugne Vais <ugnevais88@inbox.lt>
2022-09-08 20:25:19 +0300

    class work

--- day_2.py
+++ day_2.py
@@ -1,4 +1,30 @@
 my_name = "Ugne"
 print(my_name)
-print(my_name +"is fun")
+print(my_name +" is fun")
 print(type(my_name)) #to know data type
+
+a = 9001
+b = 9_000 # _ for large numbers for cosmetic reason, 1_000_000 = 1000000
+print(a,b)
+
+president = "Valdis"
+neighbor = "Valdis"
+print(id(my_name), id(president), id(neighbor), sep="\n")  # sep is what to use between , in print
+neighbor = "Voldemars"
+print(id(my_name), id(president), id(neighbor), sep="\n***\n")
+
+
+print(round(2.49), round(2.5), round(2.51))
+print(3.5 - 0.5 - 3)
+print(round(1.459230,3))
+
+my_name = input("What is your name?")
+print(f"Wow that is a nice name {my_name}")
+
+a = input ("What number you want to double?")
+double_str_a = a * 2
+print (f"Cool {a} doubled is {double_str_a}") #you get string
+
+my_float = float(a)
+float_square = my_float ** 2
+print (f"Cool {a} doubled is {float_square}") # get number
