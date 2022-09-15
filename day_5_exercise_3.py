9bc49013617494d0f1f6180e4dde168342f876cb
Ugne Vais <ugnevais88@inbox.lt>
2022-09-15 20:59:51 +0300

    day5

--- day_5_exercise_3.py
+++ day_5_exercise_3.py
@@ -9,13 +9,17 @@
 # Exception: if the words in the input are not .... bad, 
 # then the output is not ...  bad section must be changed to is good
 
+from calendar import c
+
+
 text=input("Enter your text: ")
 
-first_word=text.rfind("not")    #any difference between find and rfind
-second_word=text.rfind("bad")
+first_word = text.rfind("not")    #any difference between find and rfind
+second_word = text.rfind("bad")
 new_word = "good"
 
 if first_word > 0 and second_word > 0:
     
-    changed = text.replace(first_word, new_word)
-    print(changed)
+    # print(text.replace(first_word:first_word, "good"))
+    # text=['good' if i==first_word else i for new_word]
+    print(text)
