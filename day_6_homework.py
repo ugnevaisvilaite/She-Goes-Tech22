80734dc7f9ec2580fbb8be7165d1587321540486
Ugne Vais <ugnevais88@inbox.lt>
2022-09-20 21:59:15 +0300

    day6

---
+++ day_6_homework.py
@@ -0,0 +1,5 @@
+# Find and output the first 20 
+# (even better option to choose how many first primes we want) 
+# prime numbers in the form of a list i.e. [2,3,5,7,11, ...
+# so remember we already know how to find a single prime number 
+# from previous exercise
