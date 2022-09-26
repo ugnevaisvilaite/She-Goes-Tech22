eceb4a466f37fb427f8f31823f68c76046557d9c
Ugne Vais <ugnevais88@inbox.lt>
2022-09-26 09:20:54 +0300

    day7

--- day_7_homework.py
+++ day_7_homework.py
@@ -28,6 +28,7 @@
 
 def current_population(p0, perc, delta):
     percentage = round(perc*0.01,2)
+    print(percentage)
     full_population = p0+(p0*percentage)+delta
     return full_population
 
@@ -48,9 +49,10 @@ def get_city_year(p0,perc, delta, p):
  
         
 print(get_city_year(1000, 3, 50, 1600)) #-> 7
-print(get_city_year(1000, -3, 50, 1600)) #3% around 30 people, population increases ->76
-print(get_city_year(1000, -6, 50, 1600)) #6% around 60 people, population decreases -> -1
 
-print(get_city_year(1000, 2, -50, 5000)) #-> -1 
-print(get_city_year(1500, 5, 100, 5000)) #-> 15
-print(get_city_year(1500000, 2.5, 10000, 2000000)) #-> 10
+# print(get_city_year(1000, -3, 50, 1600)) #3% around 30 people, population increases ->76
+# print(get_city_year(1000, -6, 50, 1600)) #6% around 60 people, population decreases -> -1
+
+# print(get_city_year(1000, 2, -50, 5000)) #-> -1 
+# print(get_city_year(1500, 5, 100, 5000)) #-> 15
+# print(get_city_year(1500000, 2.5, 10000, 2000000)) #-> 10
