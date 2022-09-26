50bd60864168076eb72c4f66e705b2df40f689db
Ugne Vais <ugnevais88@inbox.lt>
2022-09-26 20:52:12 +0300

    day8

---
+++ day_8_exercise_1.py
@@ -0,0 +1,26 @@
+# # 1. What's the frequency?
+
+# Write a function: get_char_count(text) that receives a string and returns a dictionary with the number of single letter counts.
+
+# get_char_count("hubba bubba") -> {'h': 1, 'u': 2, 'b': 5, 'a': 2, ' ': 1} 
+
+# # dictionary sequence doesn't matter and the whole alphabet doesn't have to be included
+
+#  There may also be a solution with Counter from standard Python library but this is definitely not necessary, although it is very elegant smile
+#
+
+
+entered_text=input("Enter your text: ")
+
+char_count={}
+
+def get_char_count(entered_text):
+    entered_text = entered_text.replace(" ", "").lower()
+    for c in entered_text:
+        if c in char_count:
+            char_count[c] += 1
+        else:
+            char_count[c] = 1
+    return char_count
+
+print(get_char_count(entered_text))
