e2ec29c5e9a9a63381a154ef4e8127c6116c1708
Ugne Vais <ugnevais88@inbox.lt>
2022-09-27 20:57:45 +0300

    day9

--- day_9_exercise_2.py
+++ day_9_exercise_2.py
@@ -16,6 +16,20 @@
 # so function which takes any number of sequences and returns a tuple with common elements
 # get_common_elements(seq1, seq2, seq3, seq4, seq5, seq6, seq7) etc :), so just like print takes any number of values
 
+#2.a
 
 def get_common_elements(seq1, seq2, seq3):
-    
+    return tuple(set(seq1) & set(seq2) & set(seq3))
+
+print(get_common_elements("abc", ['a', 'b'], ('b', 'c')))
+
+
+#2.b
+def get_common_elements_second(*seq):
+    if len(seq) == 0:
+        return print("end")   #dont know how to make it print "end" or "not working"
+    return tuple((set(seq[0]).intersection(*seq[1:])))
+
+print(get_common_elements_second("abc", ['a', 'b', 'c'], ('b', 'c')))
+print(get_common_elements_second("572hsbd9i2", ['7', 'z', 'c', 'i'], ('b', 'c','i', '7')))
+print(get_common_elements_second("572hsbd9i2", [], ('b', 'c','i', '7')))
