5d499ad52430ea1994815b55a54fe497cfa5391a
Ugne Vais <ugnevais88@inbox.lt>
2022-09-28 16:18:46 +0300

    day9

--- day_9/day_9_homework.py
+++ day_9/day_9_homework.py
@@ -14,10 +14,24 @@
 
 # either approach is acceptable
 
+
 def check_unique_characters(arg_1, arg_2, arg_3):
     a=(set(arg_1) & set(arg_2)) - set(arg_3)
     return "".join(sorted(a))
 
 print(check_unique_characters("abcf", "fab", "boo"))
 print(check_unique_characters("abcf123", "fab123", "b2oo"))
+
+print("----------------------------------------------------------------")
+
+#VERSION 2 - LIMITLESS ARGUMENTS
+
+def check_unique_characters_limitless(*args):
+    a=(set(args[0]) & set(args[1])) - set(args[-1])
+    return "".join(sorted(a))
+
+print(check_unique_characters_limitless("abcf", "fab", "boo"))
+print(check_unique_characters_limitless("laba123", "a3","kala1", "abc"))
+print(check_unique_characters_limitless("abcf123", "fab123","fab123", "b2o3o"))
+print(check_unique_characters_limitless("abc123", "123","fab123", "b2o3o"))
 
