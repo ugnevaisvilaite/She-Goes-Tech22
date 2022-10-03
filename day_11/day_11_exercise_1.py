260475967807d62ee559ac733fd4346cbef790e9
Ugne Vais <ugnevais88@inbox.lt>
2022-10-03 20:45:02 +0300

    day11

---
+++ day_11/day_11_exercise_1.py
@@ -0,0 +1,30 @@
+# 1. The Shuffle
+
+# write  a function get_shuffled_cards()
+
+# Inside the function create  a 52-card list of tuples [("2", "diamonds ♦"), ("2", "hearts ♥"), ....., ("A", "spades ♠"), ("A", "clubs ♣")]
+
+# The function returns a shuffled set of cards - the same list with tuples but shuffled!
+
+# Find the correct method from built in random library.
+
+# you can use a loop or use something from the library
+
+# BONUS: Something can be useful from here: https://docs.python.org/3/library/itertools.html
+
+
+import random
+
+def get_shuffled_cards():
+    symbol = [2,3,4,5,6,7,8,9,10,"J","Q","K", "A"]
+    colors = ['heart', 'diamonds', 'spades', 'clubs']
+    cards = [(s, c) for s in symbol for c in colors]
+    
+    random.shuffle(cards)
+    return cards
+
+
+if __name__ == '__main__':
+    cards = get_shuffled_cards()
+    print(cards[:5])
+    print(cards[:52])
