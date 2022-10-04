101fb4652a8abdda6cd3c1aedcd3b36cd6f741f2
Ugne Vais <ugnevais88@inbox.lt>
2022-10-04 18:47:25 +0300

    day11

--- day_11/day_11_exercise_3.py
+++ day_11/day_11_exercise_3.py
@@ -1,33 +1,7 @@
+import day_11_exercise_2 as deck
 
-
-
-class Deck:
-    symbols=[2,3,4,5,6,7,8,9,10,"J","Q", "K", "A"]
-    colors= ["clubs (♣)", "diamonds (♦)", "hearts (♥)", "spades (♠)"]
-    
-    
-    def __init__(self):
-        self.available = list()
-        self.spent= []
-        
-    def __str__(self):
-        return f'Available cards {self.available} cards, Available cards {self.spent} cards'
-        
-    # def shuffle(self): #shuffle the list of available cards
-        
-    #     return self
-
-    # def get_the_cards(self):  #get the list of available cards(specific number of cards)
-        
-    #     return self
-    
-    
-if __name__ == "__main__":
-    # print(get_shuffled_cards())
-    my_deck = Deck()
-    print("----------------------------------------------------------------")
-    print(my_deck.available)
-    print("----------------------------------------------------------------")
-    print(my_deck.spent)
-    # my_deck.shuffle()
-    
+my_deck = deck.get_shuffled_cards()
+print(my_deck)
+print("------------------------------------------------")
+new_deck = deck.Deck()
+print(new_deck)
