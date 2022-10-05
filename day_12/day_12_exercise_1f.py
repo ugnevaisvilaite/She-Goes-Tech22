2d5e81e69eb661c614b665018ded565839becd67
Ugne Vais <ugnevais88@inbox.lt>
2022-10-05 15:16:04 +0300

    day12

--- day_12/day_12_exercise_1f.py
+++ day_12/day_12_exercise_1f.py
@@ -18,21 +18,17 @@
 from collections import Counter
 
 
-
 def get_word_usage(read_file,new_file, encoding="utf-8"):
     with open(read_file, encoding=encoding) as fin, open(new_file, mode="w", encoding=encoding) as fout:
         text =  fin.read()
         full_list = text.split()
         
         most_frequent_word_list = Counter(full_list)
-        max_frequency=most_frequent_word_list.most_common(1)
+        max_frequency=most_frequent_word_list.most_common(10)
         
         for x in max_frequency:
-            print(x) 
-        # max_frequency = 0
-        # if most_frequent_word_list[text] > max_frequency:
-        #     max_frequency = most_frequent_word_list[text]
-        # fout.write(max_frequency)
+            fout.write(str(x))
+
 
 read_file='day_12/sherlock_holmes_adventures.txt'
 new_file='day_12/count_list.txt'
